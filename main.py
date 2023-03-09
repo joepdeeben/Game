1ff168a859dfbc6b2f36c94921dfb3d512a134d0
joepdeeben <jldeeben@gmail.com>
2023-03-09 23:23:24 +0100

    can now pass through objects, added mapmaker

--- main.py
+++ main.py
@@ -3,10 +3,84 @@ import pygame as pg
 import numpy as np
 from matrices import *
 import keyboard
+from resources.world import *
 
 pg.init()
 
-#reejtreesing
+
+
+
+def interpreter(worldmap):
+    objectcoordinates = []
+    object = []
+    z = 2000
+    x = 0
+    for zaxis in worldmap:
+        x = 0
+        for xaxis in zaxis:
+            if xaxis == 1:
+                object.append([x, 300, z, 1])
+                object.append([x + 200, 300, z, 1])
+                object.append([x + 200, -300, z, 1])
+                object.append([x, -300, z, 1])
+                object.append([x, 300, z, 1])
+                objectcoordinates.append(object)
+                object = []
+            elif xaxis == 2:
+                object.append([x, 300, z, 1])
+                object.append([x, 300, z - 200, 1])
+                object.append([x, -300, z - 200, 1])
+                object.append([x, -300, z, 1])
+                object.append([x, 300, z, 1])
+                objectcoordinates.append(object)
+                object = []
+
+            x += 200
+        z -= 200
+    return objectcoordinates
+
+
+
+def raycaster(objectcoordinates):
+
+    t = 640 / np.dot(objectcoordinates, [0, 0, 1, 0])
+    if t < 0:
+        return objectcoordinates * 0
+    else:
+        scaled = objectcoordinates * t
+        return scaled
+
+def drawer(points, camerapos, cameradirection):
+        for point in points:
+            count = 0
+            lastx = 0
+            lasty = 0
+            for i in point:
+
+                i = i + camerapos
+                i = rotate_y(a) @ i
+
+                i = i - camerapos
+
+                i = i + camerapos
+                i = raycaster(i)
+
+                count += 1
+
+
+                if np.array_equal(np.absolute(i), [0, 0, 0, 0]):
+                    break
+                x = i[0] + 320
+                y = i[1] + 320
+
+
+
+                pg.draw.circle(background, black, (x, y), radius=3)
+                if count > 1:
+                    pg.draw.line(background, black, (lastx, lasty), (x, y), 2)
+
+                lastx = x
+                lasty = y
 
 width, height = 640, 640
 black = (0, 0, 0)
@@ -18,87 +92,39 @@ run = True
 fps = pg.time.Clock()
 
 
-
-points = []
-points.append(np.array([-300, -300, 1280, 1]))
-points.append(np.array([-300, 300, 1280, 1]))
-points.append(np.array([300, 300, 1280, 1]))
-points.append(np.array([300, -300, 1280, 1]))
-
-points2 = []
-points2.append(np.array([300, 300, 1280, 1]))
-points2.append(np.array([300, -300, 1280, 1]))
-points2.append(np.array([300, -300, 840, 1]))
-points2.append(np.array([300, 300, 840, 1]))
-points2.append(np.array([300, 300, 1280, 1]))
-
-
-objectlist = [points, points2]
-
 np.set_printoptions(suppress=True)
 
-def raycaster(objectcoordinates):
-
-    t = 640 / np.dot(objectcoordinates, [0, 0, 1, 0])
-    scaled = objectcoordinates * t
-    return scaled
 
 a = 0
-m = 0
 
+worldmap_interpreted = interpreter(world)
 
 
 camerapos = np.array([0, 0, 0, 0])
 cameradirection = np.array([0, 0, 1, 0])
 
-def draw_points(points, camerapos, cameradirection):
-    count = 0
-    print(points)
-    for object in objectlist:
-        for i in object:
-            print(i)
-            print(camerapos)
-            i = i + camerapos
-            i = rotate_y(a) @ i
-
-            print(cameradirection)
-            i = i - camerapos
-
-
-            i = i + camerapos
-            i = raycaster(i)
-            count += 1
-
-            x = i[0] + 320
-            y = i[1] + 320
-
-            pg.draw.circle(background, black, (x, y), radius=3)
-            if count > 1:
-                pg.draw.line(background, black, (lastx, lasty), (x, y), 2)
-
-            lastx = x
-            lasty = y
+print(worldmap_interpreted)
 
 while run:
     fps.tick(60)
 
     if keyboard.is_pressed('left arrow'):
-        a -= 0.01
+        a -= 0.02
         cameradirection = rotate_y(-a) @ np.array([0, 0, 1, 0])
     if keyboard.is_pressed('up arrow'):
-        camerapos = camerapos - (10 * cameradirection)
+        camerapos = camerapos - (15 * cameradirection)
         print(camerapos)
     if keyboard.is_pressed('down arrow'):
-        camerapos = camerapos + (10 * cameradirection)
+        camerapos = camerapos + (15 * cameradirection)
         print(camerapos)
     if keyboard.is_pressed('right arrow'):
-        a += 0.01
+        a += 0.02
         cameradirection = rotate_y(-a) @ np.array([0, 0, 1, 0])
 
     background = pg.display.set_mode((width, height))
     background.fill(color=white)
 
-    draw_points(points,camerapos, cameradirection)
+    drawer(worldmap_interpreted,camerapos, cameradirection)
     pg.display.flip()
 
 

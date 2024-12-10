39a5de0b0484b2cc4ae2a607cf821633f81ef0ae
joepdeeben <jldeeben@gmail.com>
2024-12-10 21:35:06 +0100

    drawing of walls through polygons

--- main.py
+++ main.py
@@ -41,48 +41,49 @@ def interpreter(worldmap):
 
 
 
-def raycaster(objectcoordinates):
+def raycaster(objectcoordinates, res):
 
-    t = 640 / np.dot(objectcoordinates, [0, 0, 1, 0])
+    t = res[0] / np.dot(objectcoordinates, [0, 0, 1, 0])
     if t < 0:
         return objectcoordinates * 0
     else:
         scaled = objectcoordinates * t
         return scaled
 
-def drawer(points, camerapos, cameradirection):
-        for point in points:
-            count = 0
-            lastx = 0
-            lasty = 0
-            for i in point:
+def drawer(points, camerapos, cameradirection, res):
+    for point in points:
+        transformed_points = []  # Store the projected 2D points for each rectangle
 
-                i = i + camerapos
-                i = rotate_y(a) @ i
+        for i in point:
+            # Apply transformations
+            i = i + camerapos
+            i = rotate_y(a) @ i
+            i = i - camerapos
+            i = i + camerapos
+            i = raycaster(i, res)
 
-                i = i - camerapos
+            # Skip invalid points
+            if np.array_equal(np.absolute(i), [0, 0, 0, 0]):
+                continue
 
-                i = i + camerapos
-                i = raycaster(i)
+            # Map 3D points to 2D screen coordinates
+            x = i[0] + res[0] / 2
+            y = i[1] + res[1] / 2
 
-                count += 1
-
-
-                if np.array_equal(np.absolute(i), [0, 0, 0, 0]):
-                    break
-                x = i[0] + 320
-                y = i[1] + 320
-
+            transformed_points.append((x, y))
+        print(transformed_points)
+        # Fill the rectangle if it has enough points
+        if len(transformed_points) >= 4: # Minimum points for a filled rectangle
+            pg.draw.polygon(background, (0, 128, 255), transformed_points)  # Fill with blue color
 
 
-                pg.draw.circle(background, black, (x, y), radius=3)
-                if count > 1:
-                    pg.draw.line(background, black, (lastx, lasty), (x, y), 2)
+        # Draw edges of the rectangle
+        for j in range(len(transformed_points)):
+            start = transformed_points[j]
+            end = transformed_points[(j + 1) % len(transformed_points)]  # Loop back to the start
+            pg.draw.line(background, black, start, end, 6)
 
-                lastx = x
-                lasty = y
-
-width, height = 640, 640
+width, height = 1920, 1080
 black = (0, 0, 0)
 white = (255, 255, 255)
 run = True
@@ -105,7 +106,11 @@ cameradirection = np.array([0, 0, 1, 0])
 
 print(worldmap_interpreted)
 
+background = pg.display.set_mode((width, height))
 while run:
+    for event in pg.event.get():  # Handle events
+        if event.type == pg.QUIT:
+            run = False  # Quit the loop when the window is closed
     fps.tick(60)
 
     if keyboard.is_pressed('left arrow'):
@@ -113,18 +118,18 @@ while run:
         cameradirection = rotate_y(-a) @ np.array([0, 0, 1, 0])
     if keyboard.is_pressed('up arrow'):
         camerapos = camerapos - (15 * cameradirection)
-        print(camerapos)
+
     if keyboard.is_pressed('down arrow'):
         camerapos = camerapos + (15 * cameradirection)
-        print(camerapos)
+
     if keyboard.is_pressed('right arrow'):
         a += 0.02
         cameradirection = rotate_y(-a) @ np.array([0, 0, 1, 0])
 
-    background = pg.display.set_mode((width, height))
     background.fill(color=white)
 
-    drawer(worldmap_interpreted,camerapos, cameradirection)
+
+    drawer(worldmap_interpreted,camerapos, cameradirection, (width, height))
     pg.display.flip()
 
 
